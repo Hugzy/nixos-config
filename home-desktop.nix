d73cc0c43b093d16716ac1a480e744fd42af4f20
frederik madsen <frede.madsen3@gmail.com>
2025-11-02 18:41:56 +0100

    fixed another syntax error

--- home-desktop.nix
+++ home-desktop.nix
@@ -1,30 +1,30 @@
 { pkgs, stable, lib, ... }:
 {
-imports = [
-  ./hardware/home-desktop.nix
-  ./configuration.nix
-];
+  imports = [
+    ./hardware/home-desktop.nix
+    ./configuration.nix
+  ];
 
-services.interception-tools = {
-  enable = true;
-  plugins = [pkgs.interception-tools-plugins.caps2esc];
-  udevmonConfig = ''
-    - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
-      DEVICE:
-        EVENTS:
-          EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
-  '';
-};
+  services.interception-tools = {
+    enable = true;
+    plugins = [pkgs.interception-tools-plugins.caps2esc];
+    udevmonConfig = ''
+      - JOB: "${pkgs.interception-tools}/bin/intercept -g $DEVNODE | ${pkgs.interception-tools-plugins.caps2esc}/bin/caps2esc | ${pkgs.interception-tools}/bin/uinput -d $DEVNODE"
+        DEVICE:
+          EVENTS:
+            EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
+    '';
+  };
 
-environment.systemPackages = with stable;
-  [
-    jq
-    brave
-    claude-code
-  ]
+  environment.systemPackages = with stable;
+    [
+      jq
+      brave
+      claude-code
+    ];
 
-networking.hostName = "home-desktop";
+  networking.hostName = "home-desktop";
 
-use-home-manager.enable = true;
+  use-home-manager.enable = true;
 
 }
